9b4841d5b54a8e15a716bbf23dbfdd7ad1d56e86
Dobi Okrasa <dobi.okrasa@gmail.com>
2017-12-10 15:37:44 +0100

    Add arrows to carousel

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 $(function(){
 	var carouselList = $("#carousel ul");
-  // setTimeout(changeSlide, 3000);
-  setInterval(changeSlide, 3000);
+  var interval;
+	function setDefaultInterval() {
+		interval = setInterval(changeSlide, 3000);
+	}
+
+	setDefaultInterval();
 
   function changeSlide() {
     carouselList.animate({'marginLeft':-1000}, 500, moveFirstSlide);
@@ -13,4 +17,26 @@ $(function(){
     lastItem.after(firstItem);
     carouselList.css({marginLeft:0});
   };
+
+// ARROWS
+	$(".fa-angle-right").click(function() {
+		clearInterval(interval);
+		changeSlide();
+		setDefaultInterval();
+	});
+
+	$(".fa-angle-left").click(function(){
+		clearInterval(interval);
+		moveLastSlide();
+		carouselList.animate({'marginLeft':0}, 500, setDefaultInterval);
+	});
+
+	function moveLastSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		firstItem.before(lastItem);
+    carouselList.css({marginLeft:-1000});
+  };
 });
+
+// CIRCLES
